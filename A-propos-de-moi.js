bba205b4268c6060ee4956b7cc25462193c6c8b3
IAlexiis <alexis.m101915@gmail.com>
2024-03-29 00:35:33 +0100

    modif

---
+++ A-propos-de-moi.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector(".btn");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    btn.classList.add("active");
+  } else {
+    btn.classList.remove("active");
+  }
+});
+
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+  });
+});
